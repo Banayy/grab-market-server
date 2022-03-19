2289ac8f08adf17682c69da07bacdaf77836a612
박성준 <parksungjun@bagseongjun-ui-MacBookAir.local>
2022-03-19 15:43:09 +0900

    heroku 배포 준비

--- index.js
+++ index.js
@@ -1,27 +1,157 @@
-var http = require("http");
-var hostname = "127.0.0.1";
-var port = 8080;
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const models = require("./models");
+const multer = require("multer");
+const upload = multer({
+  storage: multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, "uploads/");
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    },
+  }),
+});
+const port = process.env.PORT || 8080;
 
-const server = http.createServer(function (req, res) {
-  const path = req.url;
-  const method = req.method;
-  if (path === "/products") {
-    if (method === "GET") {
-      res.writeHead(200, { "Content-Type": "application/json" });
-      const products = JSON.stringify([
-        {
-          name: "농구공",
-          price: 5000,
-        },
-      ]);
-      res.end(products);
-    } else if (method === "POST") {
-      res.end("생성되었습니다!");
-    }
-  }
-  res.end("Good bye!");
+app.use(express.json());
+app.use(cors());
+app.use("/uploads", express.static("uploads"));
+
+app.get("/banners", (req, res) => {
+  models.Banner.findAll({
+    limit: 2,
+  })
+    .then((result) => {
+      res.send({
+        banners: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500), send("에러가 발생했습니다.");
+    });
 });
 
-server.listen(port, hostname);
+/* node server 종료 : ctrl + c */
+/* /PRODUCTS 에 대한 조회 API 코드 */
+/* 조회할 때는 attributes 를 통해, 가져올 정보만
+상세 페이지 조회할 때 가져올 정보를 또 설정할 수 있으므로 */
+app.get("/products", (req, res) => {
+  models.product
+    .findAll({
+      order: [["createdAt", "DESC"]],
+      attributes: [
+        "id",
+        "name",
+        "price",
+        "createdAt",
+        "seller",
+        "imageUrl",
+        "soldout",
+      ],
+    })
+    .then((result) => {
+      console.log("PRODUCTS : ", result);
+      res.send({
+        products: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send("에러 발생");
+    });
+});
 
-console.log("grab market server on!");
+app.post("/products", (req, res) => {
+  const body = req.body;
+  const { name, description, price, seller, imageUrl } = body;
+  if (!name || !description || !price || !seller || !imageUrl) {
+    res.status(400).send("모든 필드를 입력해주세요");
+  }
+  models.product
+    .create({
+      name,
+      description,
+      price,
+      seller,
+      imageUrl,
+    })
+    .then((result) => {
+      console.log("상품 생성 결과 :", result);
+      res.send({
+        result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send("상품 업로드에 문제가 발생했습니다.");
+    });
+});
+
+/* /PRODUCTS/id 에 대한 조회 API 코드 */
+app.get("/products/:id", (req, res) => {
+  const params = req.params;
+  const { id } = params;
+  models.product
+    .findOne({
+      where: {
+        id: id,
+      },
+    })
+    .then((result) => {
+      console.log("PRODUCTS : ", result);
+      res.send({
+        product: result,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(400).send("상품 조회에 에러가 발생했습니다.");
+    });
+});
+
+app.post("/image", upload.single("image"), (req, res) => {
+  const file = req.file;
+  console.log(file);
+  res.send({
+    imageUrl: file.path,
+  });
+});
+
+app.post("/purchase/:id", (req, res) => {
+  const { id } = req.params;
+  models.product
+    .update(
+      {
+        soldout: 1,
+      },
+      {
+        where: { id },
+      }
+    )
+    .then((result) => {
+      res.send({
+        result: true,
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+      res.status(500).send("에러가 발생했습니다.");
+    });
+});
+
+app.listen(port, () => {
+  console.log("그랩의 쇼핑몰 서버가 돌아가고 있습니다.");
+  models.sequelize
+    .sync()
+    .then(() => {
+      console.log("DB 연결 성공");
+    })
+    .catch((err) => {
+      console.error(err);
+      console.log("DB 연결 실패ㅠ");
+      process.exit();
+    });
+});
